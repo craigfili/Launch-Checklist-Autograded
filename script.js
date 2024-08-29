16ea83a30f1b62015718b1e2b0a513353bdb870d
craigfili <craig.filipowicz@gmail.com>
2024-08-29 17:56:18 -0500

    Latest Version

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ window.addEventListener("load", function() {
     let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
-        console.log(listedPlanets);
     }).then(function () {
-        console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
         let planetsResponse = pickPlanet(listedPlanets);
         addDestinationInfo(document, planetsResponse.name, planetsResponse.diameter,
